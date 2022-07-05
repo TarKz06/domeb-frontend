ef13d73be9c5b2643049a5616b322a5599646c8c
TarKz06 <khematat_s@elearning.cmu.ac.th>
2022-07-05 20:41:48 +0700

    auth : create component and create view

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -8,10 +8,19 @@
 
         <div class="navbar-menu">
             <div class="navbar-end">
+                <router-link to="/dashboard/leads" class="navbar-item">Leads</router-link>
+                <router-link to="/dashboard/team" class="navbar-item">Team</router-link>
+    
                 <div class="navbar-item">
                     <div class="buttons">
-                        <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
-                        <router-link to="/log-in" class="button is-light">Log in</router-link>
+                        <template v-if="!$store.state.isAuthenticated">
+                            <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+                            <router-link to="/log-in" class="button is-light">Log in</router-link>
+                        </template>
+
+                        <template v-else>
+                            <router-link to="/dashboard/my-account" class="button is-info">My account</router-link>
+                        </template>
                     </div>
                 </div>
             </div>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,13 @@ import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
 import Dashboard from '../views/dashboard/Dashboard.vue'
 import MyAccount from '../views/dashboard/MyAccount.vue'
+import Leads from '../views/dashboard/Leads.vue'
+import Lead from '../views/dashboard/Lead.vue'
+import AddLead from '../views/dashboard/AddLead.vue'
+import EditLead from '../views/dashboard/EditLead.vue'
+import AddTeam from '../views/dashboard/AddTeam.vue'
+import Team from '../views/dashboard/Team.vue'
+import AddMember from '../views/dashboard/AddMember.vue'
 
 const routes = [
   {
@@ -47,7 +54,63 @@ const routes = [
     meta: {
       requireLogin: true
     }
-  }
+  },
+  {
+    path: '/dashboard/team',
+    name: 'Team',
+    component: Team,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/team/add-member',
+    name: 'AddMember',
+    component: AddMember,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/add-team',
+    name: 'AddTeam',
+    component: AddTeam,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/leads',
+    name: 'Leads',
+    component: Leads,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/leads/add',
+    name: 'AddLead',
+    component: AddLead,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/leads/:id',
+    name: 'Lead',
+    component: Lead,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/dashboard/leads/:id/edit',
+    name: 'EditLead',
+    component: EditLead,
+    meta: {
+      requireLogin: true
+    }
+  },
 ]
 
 const router = createRouter({
